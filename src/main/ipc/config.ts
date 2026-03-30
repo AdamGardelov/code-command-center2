c4c983e802e2737ae14b3fd1a431883f6299b653
Adam Gärdelöv <adam.gardelov@wint.se>
2026-03-30 20:06:28 +0200

    Add config IPC handlers

---
+++ src/main/ipc/config.ts
@@ -0,0 +1,13 @@
+import { ipcMain } from 'electron'
+import type { ConfigService } from '../config-service'
+import type { CccConfig } from '../../shared/types'
+
+export function registerConfigIpc(configService: ConfigService): void {
+  ipcMain.handle('config:load', () => {
+    return configService.load()
+  })
+
+  ipcMain.handle('config:update', (_event, partial: Partial<CccConfig>) => {
+    return configService.update(partial)
+  })
+}
